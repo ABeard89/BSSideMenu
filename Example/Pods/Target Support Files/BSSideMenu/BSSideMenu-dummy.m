c86ad752747410c97acffc4bada4b924e4e55288
ABeard89 <anthony.beard89@gmail.com>
2017-12-18 11:30:10 +0900

    Initial Commit

---
+++ Example/Pods/Target Support Files/BSSideMenu/BSSideMenu-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BSSideMenu : NSObject
+@end
+@implementation PodsDummy_BSSideMenu
+@end
---
+++ Example/Pods/Target Support Files/Pods-BSSideMenu_Example/Pods-BSSideMenu_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BSSideMenu_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BSSideMenu_Example
+@end
